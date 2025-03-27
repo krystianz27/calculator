5f8e6e3fd1662987d289981c903031d86d6e622d
Krystian <226311@student.uek.krakow.pl>
2025-03-27 17:58:38 +0100

    header

--- generate-mps-table.js
+++ generate-mps-table.js
@@ -11,7 +11,19 @@ export function generateMPSTable() {
   thead.innerHTML = "";
   const headerRow = document.createElement("tr");
   const thLabel = document.createElement("th");
-  thLabel.textContent = "Tydzień";
+
+  const upperDiv = document.createElement("div");
+  upperDiv.textContent = "Okres";
+  upperDiv.classList.add("text-right", "block");
+
+  const lowerDiv = document.createElement("div");
+  lowerDiv.textContent = "Dane produkcyjne";
+  lowerDiv.classList.add("text-left", "block");
+
+  thLabel.appendChild(upperDiv);
+  thLabel.appendChild(lowerDiv);
+
+  // thLabel.textContent = "Tydzień";
   thLabel.classList.add("max-w-24", "py-2", "border-b", "text-right");
   headerRow.appendChild(thLabel);
 
